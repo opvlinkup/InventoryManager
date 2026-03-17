6ec4549c5787bb636ea16dd2e6c121c5989e8115
opvlinkup <146995351+opvlinkup@users.noreply.github.com>
2026-03-17 14:22:06 +0300

    chore: remove Init migration from Infrastructure project

---
+++ InventoryManager.Infrastructure/Database/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using InventoryManager.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InventoryManager.Infrastructure.Database.Configurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Categories");
+
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.Id).ValueGeneratedNever();
+
+        builder.Property(c => c.Name).IsRequired().HasMaxLength(200);
+
+        builder.HasMany(c => c.Inventories)
+            .WithOne(i => i.Category)
+            .HasForeignKey(i => i.CategoryId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(c => c.Name).IsUnique();
+    }
+}
